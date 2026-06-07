14a4c7e64d2d81e6cdc923d5a70667e8e209a38f
Hemen Karimi <hemnkarimi@gmail.com>
2026-06-08 01:21:54 +0330

    MatchEntityDone

---
+++ Footbally-Domain/Entities/MatchEntity/Enums/Status.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Footbally_Domain.Entities.MatchEntity.Enums;
+
+public enum Status
+{
+    Scheduled,
+    Finished,
+    Cancelled
+}
--- Footbally-Domain/Entities/TeamEntity/Entity/Team.cs
+++ Footbally-Domain/Entities/TeamEntity/Entity/Team.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Footbally_Domain.Entities.TeamEntity.Entity
 {
-    internal class Team
+    public class Team
     {
     }
 }
