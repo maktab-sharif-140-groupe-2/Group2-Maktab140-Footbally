51a0306b04dad3de6c8f6532cc9da3f20f6e0571
Hemen Karimi <hemnkarimi@gmail.com>
2026-06-08 01:03:11 +0330

    CreateFolderingForDomainLayer

---
+++ Footbally-Domain/Entities/PlayerEntity/Entity/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footbally_Domain.Entities.PlayerEntity.Entity
+{
+    internal class Player
+    {
+    }
+}
