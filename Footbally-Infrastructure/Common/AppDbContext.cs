1e10a0d9bf59ec25436143743d8a56ce1382ef69
Hemen Karimi <hemnkarimi@gmail.com>
2026-06-08 02:44:18 +0330

    CommitWithError

---
+++ Footbally-Infrastructure/Common/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Footbally_Infrastructure.Common;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
