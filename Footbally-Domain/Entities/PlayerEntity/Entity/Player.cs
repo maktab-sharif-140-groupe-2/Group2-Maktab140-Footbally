8d5e61e0ee697ccf24dcf9660685726f51fc4c9e
farjad <11@DESKTOP-A87MHH8>
2026-06-08 01:39:00 +0330

    PlayerEntity

---
+++ Footbally-Domain/Entities/PlayerEntity/Entity/Player.cs
@@ -0,0 +1,58 @@
+﻿using Footbally_Domain.Entities.CommonEntity.Entity;
+using Footbally_Domain.Entities.TeamEntity.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footbally_Domain.Entities.PlayerEntity.Entity
+{
+    public class Player : BaseEntity
+    {
+        public Player(string firstName, string lastName, string shirtNumber, string position, DateTime birthDate, int teamId)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            ShirtNumber = shirtNumber;
+            Position = position;
+            BirthDate = birthDate;
+            TeamId = teamId;
+        }
+        #region Properties
+        public string FirstName {  get;private set; }
+       public string LastName {  get; private set; }
+       public string ShirtNumber {  get; private set; }
+       public string Position {  get; private set; }
+       public DateTime BirthDate {  get; private set; }
+        #endregion
+        #region Navigation
+        public Team Team { get; private set; }
+        public  int TeamId { get; private set; }
+        #endregion
+        protected override void Validate()
+        {
+         if (FirstName == null)
+            {
+                throw new ArgumentNullException("Invalid FirstName");
+            }
+         if (LastName == null)
+            {
+                throw new ArgumentNullException("Invalid LastName");
+            }
+         if (ShirtNumber == null)
+            {
+                throw new ArgumentNullException("Invalid LastName");
+            }
+         if (Position == null)
+            {
+                throw new ArgumentNullException("Invalid LastName");
+            }
+         if (BirthDate == null)
+            {
+                throw new ArgumentNullException("Invalid LastName");
+            }
+
+        }
+    }
+}
---
+++ Footbally-Domain/Entities/PlayerEntity/Entity/PositionEnum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footbally_Domain.Entities.PlayerEntity.Entity
+{
+    public enum PositionEnum
+    {
+        GoalKeeper=1,
+        CenterBack,
+        RightBAck,
+        LeftBack,
+        DefendCenterMidfilder,
+        CenterMidfilder,
+        AttackingMidfilder,
+        RightWinger,
+        LeftWinger,
+        CenterForward
+
+
+    }
+}
