2552cde474f8fd5507e0d1f93a711b14b47cae3a
Hemen Karimi <hemnkarimi@gmail.com>
2026-06-08 01:46:10 +0330

    AddnumsFileInPlayerEntity

---
+++ Footbally-Domain/Entities/CommonEntity/Data/IGenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq.Expressions;
+
+namespace Footbally_Domain.Entities.CommonEntity.Data;
+
+public interface IGenericRepository<T>
+{
+    /// <summary>
+    /// دریافت همه موجودیت ها 
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="selector"></param>
+    /// <param name="tracking"></param>
+    /// <returns></returns>
+    Task<List<TResult>> QueryAsync<TResult>(Expression<Func<T, TResult>> selector, bool tracking = false);
+
+    /// <summary>
+    /// بررسی شرط برای همه موجودیت ها 
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+
+    /// <summary>
+    /// دریافت یک موجودیت با ایدی 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    Task<T?> GetByIdAsync(int id, bool tracking = false);
+
+    /// <summary>
+    /// اضافه کردن موجودیت 
+    /// </summary>
+    /// <param name="entity"></param>
+    /// <returns></returns>
+    Task<bool> AddAsync(T entity);
+
+    /// <summary>
+    /// اپدیت موجودیت
+    /// </summary>
+    /// <param name="entity"></param>
+    /// <returns></returns>
+    Task<bool> UpdateAsync(T entity);
+
+    /// <summary>
+    /// حذف نرم موجودیت 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    Task<bool> SoftDeleteAsync(int id);
+}
--- Footbally-Domain/Entities/PlayerEntity/Enums/PositionEnum.cs
+++ Footbally-Domain/Entities/PlayerEntity/Enums/PositionEnum.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Footbally_Domain.Entities.PlayerEntity.Entity
+namespace Footbally_Domain.Entities.PlayerEntity.Enums
 {
     public enum PositionEnum
     {
@@ -18,7 +18,5 @@ namespace Footbally_Domain.Entities.PlayerEntity.Entity
         RightWinger,
         LeftWinger,
         CenterForward
-
-
     }
 }
