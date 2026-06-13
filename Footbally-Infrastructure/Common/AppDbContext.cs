e9a19fad95e11173a6b8e8edba67ac42b13a7ec1
Hemen Karimi <hemnkarimi@gmail.com>
2026-06-13 23:53:18 +0330

    Nothing happend

--- Footbally-Infrastructure/Common/AppDbContext.cs
+++ Footbally-Infrastructure/Common/AppDbContext.cs
@@ -4,6 +4,8 @@ using Footbally_Domain.Entities.StandingEntity.Entity;
 using Footbally_Domain.Entities.TeamEntity.Entity;
 using System.Reflection;
 using Microsoft.EntityFrameworkCore;
+using Footbally_Domain.Entities.PlayerPerformanceEntity.Entity;
+using Footbally_Domain.Entities.UserEntity.Entity;
 
 namespace Footbally_Infrastructure.Common;
 
@@ -18,6 +20,8 @@ public class AppDbContext : DbContext
     public DbSet<Match> Matchs => Set<Match>();
     public DbSet<Standing> Standings => Set<Standing>();
     public DbSet<Team> Teams => Set<Team>();
+    public DbSet<PlayerPerformance> PlayerPerformances => Set<PlayerPerformance>();
+    public DbSet<User> Users => Set<User>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
