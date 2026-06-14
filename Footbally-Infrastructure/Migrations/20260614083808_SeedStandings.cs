642352df283e20ace63beed647f19f700a77b10e
Mahdi <Mahdi@DESKTOP-ATMOOIR>
2026-06-14 01:40:34 -0700

    Add SeedData of Standing

---
+++ Footbally-Infrastructure/Migrations/20260614083808_SeedStandings.cs
@@ -0,0 +1,315 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Footbally_Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedStandings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Standings",
+                columns: new[] { "Id", "DeletedAt", "Draw", "GoalsAgainst", "GoalsFor", "Lost", "Played", "Points", "TeamId", "UpdatedAt", "Won" },
+                values: new object[,]
+                {
+                    { 1, null, 0, 0, 0, 0, 0, 0, 1, null, 0 },
+                    { 2, null, 0, 0, 0, 0, 0, 0, 2, null, 0 },
+                    { 3, null, 0, 0, 0, 0, 0, 0, 3, null, 0 },
+                    { 4, null, 0, 0, 0, 0, 0, 0, 4, null, 0 },
+                    { 5, null, 0, 0, 0, 0, 0, 0, 5, null, 0 },
+                    { 6, null, 0, 0, 0, 0, 0, 0, 6, null, 0 },
+                    { 7, null, 0, 0, 0, 0, 0, 0, 7, null, 0 },
+                    { 8, null, 0, 0, 0, 0, 0, 0, 8, null, 0 },
+                    { 9, null, 0, 0, 0, 0, 0, 0, 9, null, 0 },
+                    { 10, null, 0, 0, 0, 0, 0, 0, 10, null, 0 },
+                    { 11, null, 0, 0, 0, 0, 0, 0, 11, null, 0 },
+                    { 12, null, 0, 0, 0, 0, 0, 0, 12, null, 0 },
+                    { 13, null, 0, 0, 0, 0, 0, 0, 13, null, 0 },
+                    { 14, null, 0, 0, 0, 0, 0, 0, 14, null, 0 },
+                    { 15, null, 0, 0, 0, 0, 0, 0, 15, null, 0 },
+                    { 16, null, 0, 0, 0, 0, 0, 0, 16, null, 0 },
+                    { 17, null, 0, 0, 0, 0, 0, 0, 17, null, 0 },
+                    { 18, null, 0, 0, 0, 0, 0, 0, 18, null, 0 },
+                    { 19, null, 0, 0, 0, 0, 0, 0, 19, null, 0 },
+                    { 20, null, 0, 0, 0, 0, 0, 0, 20, null, 0 },
+                    { 21, null, 0, 0, 0, 0, 0, 0, 21, null, 0 },
+                    { 22, null, 0, 0, 0, 0, 0, 0, 22, null, 0 },
+                    { 23, null, 0, 0, 0, 0, 0, 0, 23, null, 0 },
+                    { 24, null, 0, 0, 0, 0, 0, 0, 24, null, 0 },
+                    { 25, null, 0, 0, 0, 0, 0, 0, 25, null, 0 },
+                    { 26, null, 0, 0, 0, 0, 0, 0, 26, null, 0 },
+                    { 27, null, 0, 0, 0, 0, 0, 0, 27, null, 0 },
+                    { 28, null, 0, 0, 0, 0, 0, 0, 28, null, 0 },
+                    { 29, null, 0, 0, 0, 0, 0, 0, 29, null, 0 },
+                    { 30, null, 0, 0, 0, 0, 0, 0, 30, null, 0 },
+                    { 31, null, 0, 0, 0, 0, 0, 0, 31, null, 0 },
+                    { 32, null, 0, 0, 0, 0, 0, 0, 32, null, 0 },
+                    { 33, null, 0, 0, 0, 0, 0, 0, 33, null, 0 },
+                    { 34, null, 0, 0, 0, 0, 0, 0, 34, null, 0 },
+                    { 35, null, 0, 0, 0, 0, 0, 0, 35, null, 0 },
+                    { 36, null, 0, 0, 0, 0, 0, 0, 36, null, 0 },
+                    { 37, null, 0, 0, 0, 0, 0, 0, 37, null, 0 },
+                    { 38, null, 0, 0, 0, 0, 0, 0, 38, null, 0 },
+                    { 39, null, 0, 0, 0, 0, 0, 0, 39, null, 0 },
+                    { 40, null, 0, 0, 0, 0, 0, 0, 40, null, 0 },
+                    { 41, null, 0, 0, 0, 0, 0, 0, 41, null, 0 },
+                    { 42, null, 0, 0, 0, 0, 0, 0, 42, null, 0 },
+                    { 43, null, 0, 0, 0, 0, 0, 0, 43, null, 0 },
+                    { 44, null, 0, 0, 0, 0, 0, 0, 44, null, 0 },
+                    { 45, null, 0, 0, 0, 0, 0, 0, 45, null, 0 },
+                    { 46, null, 0, 0, 0, 0, 0, 0, 46, null, 0 },
+                    { 47, null, 0, 0, 0, 0, 0, 0, 47, null, 0 },
+                    { 48, null, 0, 0, 0, 0, 0, 0, 48, null, 0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 23);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 24);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 25);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 26);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 27);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 28);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 29);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 30);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 31);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 32);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 33);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 34);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 35);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 36);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 37);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 38);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 39);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 40);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 41);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 42);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 43);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 44);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 45);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 46);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 47);
+
+            migrationBuilder.DeleteData(
+                table: "Standings",
+                keyColumn: "Id",
+                keyValue: 48);
+        }
+    }
+}
--- Footbally-Infrastructure/ModelConfiguration/StandingModelBuilderConfiguration.cs
+++ Footbally-Infrastructure/ModelConfiguration/StandingModelBuilderConfiguration.cs
@@ -8,10 +8,57 @@ public class StandingModelBuilderConfiguration : BaseModelConfiguration<Standing
 {
     protected override void ApplyEntityConfiguration(EntityTypeBuilder<Standing> builder)
     {
-        
-            
 
 
-
+        builder.HasData(
+     new { Id = 1, TeamId = 1, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 2, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 3, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 4, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 5, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 6, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 7, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 8, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 9, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 10, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 11, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 12, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 13, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 14, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 15, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 16, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 17, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 18, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 19, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 20, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 21, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 22, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 23, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 24, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 25, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 26, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 27, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 28, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 29, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 30, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 31, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 32, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 33, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 34, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 35, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 36, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 37, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 38, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 39, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 40, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 41, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 42, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 43, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 44, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 45, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 46, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 47, TeamId = 2, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 },
+     new { Id = 48, TeamId = 48, Played = 0, Won = 0, Lost = 0, Draw = 0, GoalsFor = 0, GoalsAgainst = 0, Points = 0 });
+  
     }
 }
