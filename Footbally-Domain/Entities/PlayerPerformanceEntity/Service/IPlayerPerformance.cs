4e82cc7f1ef6ad31308d6410166e92d19ba038c7
Mahdi <Mahdi@DESKTOP-ATMOOIR>
2026-06-14 11:14:25 -0700

    Completed Performance Service

--- Footbally-Domain/Entities/PlayerPerformanceEntity/Service/IPlayerPerformance.cs
+++
@@ -1,9 +0,0 @@
-﻿using Footbally_Common.Dto.Request.PlayerPerformance;
-
-namespace Footbally_Domain.Entities.PlayerPerformanceEntity.Service;
-
-public interface IPlayerPerformance
-{
-    Task<bool> Create(PlayerPerformanceRequestDto request);
-
-}
---
+++ Footbally-Service/Performance/PlayerPerformanceService.cs
@@ -0,0 +1,26 @@
+﻿using Footbally_Common.Dto.Request.PlayerPerformance;
+using Footbally_Domain.Entities.PlayerPerformanceEntity.Data;
+using Footbally_Domain.Entities.PlayerPerformanceEntity.Entity;
+using Footbally_Domain.Entities.PlayerPerformanceEntity.Service;
+
+namespace Footbally_Service.PlayerPerformance;
+
+public class PlayerPerformanceService : IPlayerPerformanceService
+{
+    private readonly IPlayerPerformanceRepository _playerPerformanceRepository;
+
+    public PlayerPerformanceService(IPlayerPerformanceRepository playerPerformanceRepository)
+    {
+        _playerPerformanceRepository = playerPerformanceRepository;
+    }
+
+    public async Task<bool> Add(PlayerPerformanceRequestDto request)
+    {
+        if (request.AssistPlayerId == request.GoalPlayerId)
+            throw new Exception("Goal And Assist Player Can't be One Person");
+
+        var performance = new Footbally_Domain.Entities.PlayerPerformanceEntity.Entity.PlayerPerformance(request.MatchId, request.GoalPlayerId, request.AssistPlayerId);
+
+        return await _playerPerformanceRepository.AddAsync(performance);
+    }
+}
